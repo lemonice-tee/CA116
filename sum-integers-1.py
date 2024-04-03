a06865adb8dde5dca7f36b71bb0a48390db864c4
⋆｡˚ ☁︎ ˚｡  lemonice_tee⋆｡˚☽˚｡⋆ <145129046+lemonice-tee@users.noreply.github.com>
2024-04-03 13:16:03 +0100

    Add files via upload

---
+++ sum-integers-1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+total = 0
+a = sys.stdin.readline()
+while a != "":
+  total = total + int(a)
+  a = sys.stdin.readline()
+
+print(total)
+
+#    message = list[i]
+#    total = total + list[i]
+# while smth != " "
